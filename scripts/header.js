4404e5bdc8d191b58fbb73f134875f672464679a
owl OH <d0440478@fcu.edu.tw>
2018-01-19 00:11:27 +0800

    no message

---
+++ scripts/header.js
@@ -0,0 +1,31 @@
+header ='<nav class="navbar navbar-expand-md bg-dark navbar-dark">\
+        <a class="navbar-brand" href="index.html">Home</a>\
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">\
+            <span class="navbar-toggler-icon"></span>\
+        </button>\
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">\
+            <ul class="navbar-nav">\
+                <li class="nav-item">\
+                    <a class="nav-link" href="page_noodle.html">麵類</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="page_rice.html">飯類</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="page_drink.html">飲料</a>\
+                </li>\
+            </ul>\
+            <ul class="navbar-nav ml-auto mr-5">\
+                <li class="nav-item dropdown">\
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">\
+                        組員名單\
+            </a>\
+                    <div class="dropdown-menu">\
+                        <a class="dropdown-item" href="">STAN  41號</a>\
+                        <a class="dropdown-item" href="">YUN  23號</a>\
+                    </div>\
+                </li>\
+            </ul>\
+        </div>\
+    </nav>';
+document.write(header);
